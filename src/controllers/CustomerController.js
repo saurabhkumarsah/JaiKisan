6dbc2e70c998cd4bfea2f5034795f1a2ed9d3b1f
Saurabh <saurabhsahofficial@gmail.com>
2023-05-28 15:11:13 +0530

    Add Router And it's controller Create, Get And Delete

--- src/controllers/CustomerController.js
+++ src/controllers/CustomerController.js
@@ -1,10 +1,64 @@
 const customerModel = require('../models/CustomerModel.js')
 
-const getCustomerList = async (req,res) => {
+const createCustomer = async (req, res) => {
     try {
-        const data = await customerModel.find({ status: "ACTIVE" })
-        res.status(200).send({ Status: true, Data: data })
+
+        const data = req.body
+        const { firstName, lastName, mobileNumber, DOB, emailId, address, customerId, status } = req.body
+
+        if (!firstName) return res.status(404).send({ Status: false, Message: "Please, Provide Your First Name" })
+        if (!lastName) return res.status(404).send({ Status: false, Message: "Please, Provide Your Last Name" })
+        if (!mobileNumber) return res.status(404).send({ Status: false, Message: "Please, Provide Your Mobile Number" })
+        if (mobileNumber.length !== 10) return res.status(404).send({ Status: false, Message: "Please, Provide valid mobile number" })
+        if (!DOB) return res.status(404).send({ Status: false, Message: "Please, Provide Your Date of Birth" })
+        // if (DOB != "0000-00-00") return res.status(404).send({ Status: false, Message: "Please, Provide Your Date of Birth in YYYY-MM-DD format" })
+
+        if (!emailId) return res.status(404).send({ Status: false, Message: "Please, Provide Your Email Id" })
+        if (!address) return res.status(404).send({ Status: false, Message: "Please, Provide Your Address" })
+        if (!customerId) return res.status(404).send({ Status: false, Message: "Please, Provide Your Date of Birth" })
+        if (!status) return res.status(404).send({ Status: false, Message: "Please, Provide Status" })
+        if ((status !== "ACTIVE") && (status !== "INACTIVE")) return res.status(404).send({ Status: false, Message: "Please, write status ACTIVE or INACTIVE" })
+
+        const saveData = await customerModel.create(data)
+        res.status(200).send({ Status: true, Message: "Customer Account Created Successfully", Data: saveData })
+
     } catch (error) {
-        res.status(500).send({Status: false, Message: error.message})
+
+        res.status(500).send({ Status: false, Message: error.message })
+
     }
 }
+
+const getCustomerList = async (req, res) => {
+    try {
+
+        const data = await customerModel.find({ status: "ACTIVE" })
+        res.status(200).send({ Status: true, Data: data })
+
+    } catch (error) {
+
+        res.status(500).send({ Status: false, Message: error.message })
+
+    }
+}
+
+const deleteCustomer = async (req, res) => {
+    try {
+
+        const data = req.body
+        const condition = Object.keys(data)
+        if (condition.length === 0) return res.status(404).send({ Status: false, Message: "Please, Provide Detail of Custormer" })
+        const saveData = await customerModel.updateMany(data, { isDeleted: true })
+        if (saveData.modifiedCount === 0) return res.status(404).send({ Status: false, Message: "Data not found" })
+        res.status(200).send({ Status: true, Message: "Customer Account Deleted Successfully" })
+
+    } catch (error) {
+
+        res.status(500).send({ Status: false, Message: error.message })
+
+    }
+}
+
+module.exports.createCustomer = createCustomer
+module.exports.getCustomerList = getCustomerList
+module.exports.deleteCustomer = deleteCustomer
